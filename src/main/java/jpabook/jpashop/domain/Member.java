46e2bb39629f17b4de9760632b9a47f279f6c4b6
YunChan-Oh <oyunchan9@gmail.com>
2024-05-06 23:56:00 +0900

    엔티티 클래스 1

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,20 +1,19 @@
-package jpabook.jpashop;
+package jpabook.jpashop.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
 public class Member {
-
     @Id
     @GeneratedValue
     private Long id;
 
-    private String username;
+    private String name;
+
+    private Address address;
+
+    private List<Order> orders = new ArrayList<Order>();
 
 }
