9ba123f0e373c2119a361c7014750184e3a28a07
YunChan-Oh <oyunchan9@gmail.com>
2024-05-08 09:57:41 +0900

    엔티티 설계시 주의점

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -37,6 +37,17 @@ public class Order {
 
     public void setMember(Member member) {
         this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
     }
 
 }
