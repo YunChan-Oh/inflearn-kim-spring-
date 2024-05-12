78c2b3afc5a5c3dc79ee51f8e399b96579f5c6f6
YunChan-Oh <oyunchan9@gmail.com>
2024-05-13 02:57:24 +0900

    회원 서비스 full 개발(인젝션 수정)

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -2,18 +2,29 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
+//@AllArgsConstructor
+@RequiredArgsConstructor //final의 필드를 가지고 생성자 만듬
 public class MemberService {
 
-    @Autowired
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
+
+//    @Autowired //하나면 굳이 어노테이션 필요없음
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
 
     //회원 가입
+    @Transactional //기본적으로 readonly는 false
     public Long join(Member member) {
         validateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
@@ -21,6 +32,7 @@ public class MemberService {
 
     }
 
+    //멀티 스레드를 고려하여 db에 unique조건도 잡아주기
     private void validateDuplicateMember(Member member) {
         List<Member> findMembers = memberRepository.findByName(member.getName());
         if(!findMembers.isEmpty()) {
@@ -28,10 +40,12 @@ public class MemberService {
         }
     }
     //회원 전체 조회
+    @Transactional(readOnly = true)
     public List<Member> findMembers(){
         return memberRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public Member findOne(Long memberId){
         return memberRepository.findOne(memberId);
     }
